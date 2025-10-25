75c2990e89196b68f4a790691b0e4df7bfff7b8a
Puhcoder16 <122073899+Puhcoder16@users.noreply.github.com>
2025-10-25 12:58:47 +0700

    Update ProtectedRoute.tsx

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -10,10 +10,6 @@ const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
     return <div className="flex justify-center items-center h-screen"><p>Loading...</p></div>;
   }
 
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-
   return children;
 };
 
